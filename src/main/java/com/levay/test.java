92b5057e5499e99c15a0940a0a3753b1c2cd5dc7
levey <2261496171@qq.com>
2025-07-26 07:50:35 +0800

    第二次提交

--- src/main/java/com/levay/test.java
+++ src/main/java/com/levay/test.java
@@ -4,6 +4,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("第一次提交");
+        System.out.println("第二次提交");
         System.out.println("Hello World!");
     }
 
